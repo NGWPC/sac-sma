9d79c44ac7a768b2c8379c3d9bb32465e68f33f4
Mohammed Karim <mohammed.r.karim@rtx.com>
2026-07-20 12:08:49 -0700

    new variable to compute total soil mositure

--- src/bmi/bmi_sac.f90
+++ src/bmi/bmi_sac.f90
@@ -99,7 +99,7 @@ module bmi_sac_module
 
   ! Exchange items
   integer, parameter :: input_item_count = 3
-  integer, parameter :: output_item_count = 17
+  integer, parameter :: output_item_count = 19
   character (len=BMI_MAX_VAR_NAME), target, &
        dimension(input_item_count) :: input_items
   character (len=BMI_MAX_VAR_NAME), target, &
@@ -174,6 +174,8 @@ contains
     output_items(15) = 'uzsmc_ch'  ! upper zone storage content change (m)
     output_items(16) = 'precip_out'  ! rain+melt liquid input (mm/s)
     output_items(17) = 'qg_m3_per_s'
+    output_items(18) = "totsmc"     ! Total pervious soil-water storage (m)
+    output_items(19) = "totsmc_ch"  ! Timestep change in total storage (m)
 
     names => output_items
     bmi_status = BMI_SUCCESS
@@ -310,7 +312,7 @@ contains
 
     select case(name)
     case('tair', 'precip', 'pet', &                  ! input vars
-         'qs', 'qg', 'tci', 'eta', 'tci_giuh', 'uzsmc', 'uzsmc_ch', &                ! output vars
+         'qs', 'qg', 'tci', 'eta', 'tci_giuh', 'uzsmc', 'uzsmc_ch', 'totsmc', 'totsmc_ch', &                ! output vars
          'roimp','sdro','ssur','sif','bfs','bfp', 'bfncc', 'nwm_ponded_depth', 'precip_out', 'qg_m3_per_s')
        grid = 0
        bmi_status = BMI_SUCCESS
@@ -606,7 +608,7 @@ contains
 
     select case(name)
     case('tair', 'precip', 'pet',  &                ! input vars
-         'qs', 'qg', 'tci', 'eta', 'tci_giuh', 'nwm_ponded_depth', 'uzsmc', 'uzsmc_ch', &                ! output vars
+         'qs', 'qg', 'tci', 'eta', 'tci_giuh', 'nwm_ponded_depth', 'uzsmc', 'uzsmc_ch', 'totsmc', 'totsmc_ch', &                ! output vars
          'roimp','sdro','ssur','sif','bfs','bfp', 'bfncc', 'precip_out', 'qg_m3_per_s')
        type = "real"
        bmi_status = BMI_SUCCESS
@@ -765,11 +767,9 @@ contains
     case("rserv")
        units = "mm"
        bmi_status = BMI_SUCCESS
-    case("uzsmc")
-       units = "mm"
-       bmi_status = BMI_SUCCESS
-    case("uzsmc_ch")
-       units = "mm"
+    case("uzsmc", "uzsmc_ch", "totsmc", "totsmc_ch")
+       ! Stored internally in mm but exposed through BMI in m.
+       units = "m"
        bmi_status = BMI_SUCCESS
     case default
        units = "-"
@@ -842,6 +842,12 @@ contains
        bmi_status = BMI_SUCCESS
     case("uzsmc_ch")
        size = sizeof(this%model%modelvar%uzsmc_ch(1))
+       bmi_status = BMI_SUCCESS
+    case("totsmc")
+       size = sizeof(this%model%modelvar%totsmc(1))
+       bmi_status = BMI_SUCCESS
+    case("totsmc_ch")
+       size = sizeof(this%model%modelvar%totsmc_ch(1))
        bmi_status = BMI_SUCCESS
     case("uztwm")
        size = sizeof(this%model%parameters%uztwm(1))
@@ -1058,6 +1064,12 @@ contains
        bmi_status = BMI_SUCCESS
     case("uzsmc_ch")
        dest(1) = this%model%modelvar%uzsmc_ch(1)/1000.0 !convert mm to m
+       bmi_status = BMI_SUCCESS
+    case("totsmc")
+       dest(1) = this%model%modelvar%totsmc(1) / 1000.0 !convert mm to m
+       bmi_status = BMI_SUCCESS
+    case("totsmc_ch")
+       dest(1) = this%model%modelvar%totsmc_ch(1) / 1000.0 !convert mm to m
        bmi_status = BMI_SUCCESS
     case("eta")
        dest(1) = this%model%modelvar%eta(1)
@@ -1355,10 +1367,10 @@ contains
        this%model%modelvar%nwm_ponded_depth(1) = src(1)
        bmi_status = BMI_SUCCESS
     case("uzsmc")
-       this%model%modelvar%uzsmc(1) = src(1)
+       this%model%modelvar%uzsmc(1) = src(1) * 1000.0
        bmi_status = BMI_SUCCESS
     case("uzsmc_ch")
-       this%model%modelvar%uzsmc_ch(1) = src(1)
+       this%model%modelvar%uzsmc_ch(1) = src(1) * 1000.0
        bmi_status = BMI_SUCCESS
     case("eta")
        this%model%modelvar%eta(1) = src(1)
